fc2891ffb03c9f3e2ee670ac6ea7a6cd009551c5
anoopraju31 <anoopraju31@gmail.com>
2024-01-25 14:54:58 +0530

    Navbar - initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import EnhancedReveneCard from './components/EnhancedReveneCard'
-import RevenueCard, { RevenueCardWithBoxShadow } from './components/RevenueCard'
+import Navbar from './components/Navbar'
 
 const App = () => {
 	return (
 		<div className='max-w-sm mx-auto my-20 flex flex-col gap-10'>
-			<RevenueCard title='Amount Pending' amount='₹92.312.20' orderCount={13} />
-			<RevenueCardWithBoxShadow
-				title='Amount Pending'
-				amount='₹92.312.20'
-				orderCount={13}
-			/>
-			<EnhancedReveneCard
-				title='Next Payout'
-				amount='₹2,312.23'
-				orderCount={23}
-				textColor='!text-black-100'
-				payoutDate='Today, 04:00PM'
-			/>
+			<Navbar />
 		</div>
 	)
 }
