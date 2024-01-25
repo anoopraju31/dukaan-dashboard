a87d1b5447bd31041c556f0a979d74402d1d7b41
anoopraju31 <anoopraju31@gmail.com>
2024-01-25 16:53:02 +0530

    Search Input - styled

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,21 @@ import SearchInput from './components/SearchInput'
 const App = () => {
 	return (
 		<div className='max-w-sm mx-auto my-20 flex flex-col gap-10'>
-			<SearchInput />
+			<SearchInput
+				placeholder='Search features, tutorials, etc...'
+				label='search'
+				name='search'
+				id='search'
+				variant='outlined'
+			/>
+
+			<SearchInput
+				placeholder='Search features, tutorials, etc...'
+				label='search'
+				name='search'
+				id='search'
+				variant='primary'
+			/>
 		</div>
 	)
 }
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,5 +1,41 @@
-const SearchInput = () => {
-	return <div>SearchInput</div>
+import { FC } from 'react'
+import Search from './svgs/Search'
+
+type SearchInputType = {
+	variant?: 'outlined' | 'primary'
+	label: string
+	id: string
+	name: string
+	placeholder: string
+}
+
+const SearchInput: FC<SearchInputType> = (props) => {
+	const { variant = 'primary', label, id, name, placeholder } = props
+	const style = {
+		outlined: 'bg-black-100 border border-black-85',
+		primary: 'bg-black-95',
+	}
+
+	return (
+		<>
+			<label htmlFor='search' className='sr-only'>
+				{label}
+			</label>
+
+			<div
+				className={`py-[10px] px-4 flex items-center gap-2 rounded ${style[variant]}`}>
+				<Search />
+
+				<input
+					type='text'
+					id={id}
+					name={name}
+					placeholder={placeholder}
+					className='text-black-50 text-[15px] leading--[22px] flex-shrink-0 flex-1 bg-transparent outline-none border-none'
+				/>
+			</div>
+		</>
+	)
 }
 
 export default SearchInput
