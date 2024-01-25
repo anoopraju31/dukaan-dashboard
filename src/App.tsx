54d9184840dffd1af8cd02ce8e0c5eaca281dc97
anoopraju31 <anoopraju31@gmail.com>
2024-01-25 17:01:57 +0530

    Header - initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import IconWrapper from './components/IconWrapper'
-import Menu from './components/svgs/Menu'
-import Message from './components/svgs/Message'
+import Header from './components/svgs/Header'
 
 const App = () => {
 	return (
-		<div className='max-w-sm mx-auto my-20 flex flex-col gap-10'>
-			<IconWrapper>
-				<Message />
-			</IconWrapper>
-
-			<IconWrapper>
-				<Menu />
-			</IconWrapper>
+		<div className=''>
+			<Header />
 		</div>
 	)
 }
