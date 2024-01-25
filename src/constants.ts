75caaea4f8b7b4438c83e9ff32145ade3415b879
anoopraju31 <anoopraju31@gmail.com>
2024-01-25 16:10:08 +0530

    NAv items constants added

---
+++ src/constants.ts
@@ -0,0 +1,80 @@
+import { FC } from 'react'
+import {
+	Analytics,
+	Appearance,
+	Audience,
+	Delivery,
+	Discounts,
+	Home,
+	Marketing,
+	Orders,
+	Payouts,
+	Plugins,
+	Products,
+} from './components/svgs'
+
+type NavItemType = {
+	id: string
+	Icon: FC
+	title: string
+	isCurrentPage?: boolean
+}
+
+export const navItems: NavItemType[] = [
+	{
+		id: '1',
+		Icon: Home,
+		title: 'home',
+	},
+	{
+		id: '2',
+		Icon: Orders,
+		title: 'Orders',
+	},
+	{
+		id: '3',
+		Icon: Products,
+		title: 'Products',
+	},
+	{
+		id: '4',
+		Icon: Delivery,
+		title: 'Delivery',
+	},
+	{
+		id: '5',
+		Icon: Marketing,
+		title: 'Marketing',
+	},
+	{
+		id: '6',
+		Icon: Analytics,
+		title: 'Analytics',
+	},
+	{
+		id: '7',
+		Icon: Payouts,
+		title: 'Payouts',
+		isCurrentPage: true,
+	},
+	{
+		id: '8',
+		Icon: Discounts,
+		title: 'Discounts',
+	},
+	{
+		id: '9',
+		Icon: Audience,
+		title: 'Audience',
+	},
+	{
+		id: '10',
+		Icon: Appearance,
+		title: 'Appearance',
+	},
+	{
+		id: '11',
+		Icon: Plugins,
+		title: 'Plugins',
+	},
+]
