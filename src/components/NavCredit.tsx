90fe510df8a05986f59113173562a1fff5496448
anoopraju31 <anoopraju31@gmail.com>
2024-01-25 16:25:18 +0530

    Nav Credit - styled

--- src/components/NavCredit.tsx
+++ src/components/NavCredit.tsx
@@ -1,7 +1,19 @@
 import { FC } from 'react'
+import Wallet from './svgs/Wallet'
 
 const NavCredit: FC = () => {
-	return <div>NavCredit</div>
+	return (
+		<div className='px-3 py-[6px] flex items-center gap-3 rounded bg-navbar-secondary'>
+			<Wallet />
+
+			<div className='flex flex-col gap-[2px] text-black-100 font-inter'>
+				<p className='text-[13px] leading-4 opacity-80 whitespace-nowrap'>
+					Available credits
+				</p>
+				<p className='font-medium text-base'> 222.10 </p>
+			</div>
+		</div>
+	)
 }
 
 export default NavCredit
