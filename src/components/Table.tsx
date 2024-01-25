08ba614c00341c20b759f18f7658d35c94a349d0
anoopraju31 <anoopraju31@gmail.com>
2024-01-25 18:20:04 +0530

    Table - table head styled

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,7 +1,30 @@
 import { FC } from 'react'
 
 const Table: FC = () => {
-	return <div>Table</div>
+	return (
+		<table className='w-full text-sm text-black-30 font-medium'>
+			<thead className='text-left'>
+				<tr>
+					<th className='px-3 py-[10px] bg-black-95 whitespace-nowrap rounded-s'>
+						Order ID
+					</th>
+					<th className='px-3 py-[10px] bg-black-95 whitespace-nowrap'>
+						Status
+					</th>
+					<th className='px-3 py-[10px] bg-black-95 whitespace-nowrap'>
+						Transaction ID
+					</th>
+					<th className='px-3 py-[10px] bg-black-95 whitespace-nowrap'>
+						Refund Date
+					</th>
+					<th className='px-3 py-[10px] bg-black-95 whitespace-nowrap text-right rounded-e'>
+						Order Amount
+					</th>
+				</tr>
+			</thead>
+			<tbody></tbody>
+		</table>
+	)
 }
 
 export default Table
