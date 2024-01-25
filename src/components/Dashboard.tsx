557ddc147e464fedd246734f6c1351449c84dad1
anoopraju31 <anoopraju31@gmail.com>
2024-01-25 18:44:32 +0530

    Dashboard Transactions - initial

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,10 +1,13 @@
 import { FC } from 'react'
 import DashboardHeader from './DashboardHeader'
+import DashboardTransactions from './DashboardTransactions'
 
 const Dashboard: FC = () => {
 	return (
 		<main className='w-full p-3 md:p-8 flex flex-col gap-8'>
 			<DashboardHeader />
+
+			<DashboardTransactions />
 		</main>
 	)
 }
---
+++ src/components/DashboardTransactions.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+const DashboardTransactions: FC = () => {
+	return <div>DashboardTransactions</div>
+}
+
+export default DashboardTransactions
