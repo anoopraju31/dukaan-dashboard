b42f13ca693785ecbbd8f3e654beb1dfdd999925
anoopraju31 <anoopraju31@gmail.com>
2024-01-25 18:40:29 +0530

    Dashboard Header - initial

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,12 @@
 import { FC } from 'react'
+import DashboardHeader from './DashboardHeader'
 
 const Dashboard: FC = () => {
-	return <div>Dashboard</div>
+	return (
+		<main className='w-full p-3 md:p-8 flex flex-col gap-8'>
+			<DashboardHeader />
+		</main>
+	)
 }
 
 export default Dashboard
---
+++ src/components/DashboardHeader.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+const DashboardHeader: FC = () => {
+	return <div>DashboardHeader</div>
+}
+
+export default DashboardHeader
