9dcc72e9728762d277bb5282256c2917db48beb3
anoopraju31 <anoopraju31@gmail.com>
2024-01-25 16:44:30 +0530

    Search Input - initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import Navbar from './components/Navbar'
+import SearchInput from './components/SearchInput'
 
 const App = () => {
 	return (
 		<div className='max-w-sm mx-auto my-20 flex flex-col gap-10'>
-			<Navbar />
+			<SearchInput />
 		</div>
 	)
 }
