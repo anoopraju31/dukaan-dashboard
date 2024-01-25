0aa9190fb192191675290d045b329c252a68871c
anoopraju31 <anoopraju31@gmail.com>
2024-01-25 15:31:04 +0530

    Nav Item - initial

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,16 @@
+import NavItem from './NavItem'
 import NavbarStoreProfile from './NavbarStoreProfile'
 
 const Navbar = () => {
 	return (
 		<nav className='sticky top-0 w-full min-w-60 py-4 px-[10px] flex flex-col gap-6 bg-navbar min-h-screen'>
 			<NavbarStoreProfile />
+			<ul className='flex-1 flex flex-col gap-1'>
+				<NavItem />
+				<NavItem />
+				<NavItem />
+				<NavItem />
+			</ul>
 		</nav>
 	)
 }
