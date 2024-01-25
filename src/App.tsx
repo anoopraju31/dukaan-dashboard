8ddeb56b6636720067696050041b61a23b5d89df
anoopraju31 <anoopraju31@gmail.com>
2024-01-25 18:37:46 +0530

    App - refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import DropDown from './components/DropDown'
+import { FC } from 'react'
+import Header from './components/svgs/Header'
+import Navbar from './components/Navbar'
 
-const App = () => {
+const App: FC = () => {
 	return (
-		<div className='p-10 flex gap-5'>
-			<DropDown />
+		<div className='flex bg-black-98'>
+			<div className='hidden lg:block'>
+				<Navbar />
+			</div>
+			<div className='flex-1 relative'>
+				<Header />
+			</div>
 		</div>
 	)
 }
