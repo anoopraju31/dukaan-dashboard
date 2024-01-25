9e8c4a4dc214300ae8ab1ebf21b12c7da17c4421
anoopraju31 <anoopraju31@gmail.com>
2024-01-25 18:17:34 +0530

    Tab Button - styled

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import TabButton from './components/TabButton'
 
 const App = () => {
 	return (
-		<div className=''>
-			<TabButton />
+		<div className='p-10 flex gap-5'>
+			<TabButton title='Payouts' quantity={22} />
+			<TabButton title='Refunds' quantity={6} isActive />
 		</div>
 	)
 }
--- src/components/TabButton.tsx
+++ src/components/TabButton.tsx
@@ -1,5 +1,27 @@
-const TabButton = () => {
-	return <div>TabButton</div>
+import { FC } from 'react'
+
+type TabButtonType = {
+	title: string
+	quantity: number
+	isActive?: boolean
+	handleClick?: () => null
+}
+
+const TabButton: FC<TabButtonType> = (props) => {
+	const { title, quantity, isActive, handleClick } = props
+
+	return (
+		<button
+			type='button'
+			onClick={handleClick}
+			className={`py-[6px] px-4 rounded-full flex items-center gap-[10px] font-inter text-sm font-medium hover:opacity-80 ${
+				isActive
+					? 'bg-primary-blue text-black-100'
+					: 'bg-black-90 text-black-50'
+			}`}>
+			{title} ({quantity})
+		</button>
+	)
 }
 
 export default TabButton
